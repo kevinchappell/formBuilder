2644ae92aed72f0fb1c21ec8fe46be67fa4c1eaf
Kevin Chappell <kevin.b.chappell@gmail.com>
2016-09-03 16:29:12 +0100

    Add polyfills.js to formRender

--- dist/form-builder.js
+++ dist/form-builder.js
@@ -5,6 +5,52 @@ Author: Kevin Chappell <kevin.b.chappell@gmail.com>
 */
 'use strict';
 
+// Element.remove() polyfill
+
+if (!('remove' in Element.prototype)) {
+  Element.prototype.remove = function () {
+    if (this.parentNode) {
+      this.parentNode.removeChild(this);
+    }
+  };
+}
+
+// Event polyfill
+if (typeof Event !== 'function') {
+  (function () {
+    window.Event = function (evt) {
+      var event = document.createEvent('Event');
+      event.initEvent(evt, true, true);
+      return event;
+    };
+  })();
+}
+
+// Object.assign polyfill
+if (typeof Object.assign != 'function') {
+  Object.assign = function (target) {
+    'use strict';
+
+    if (target == null) {
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    target = Object(target);
+    for (var index = 1; index < arguments.length; index++) {
+      var source = arguments[index];
+      if (source != null) {
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+    }
+    return target;
+  };
+}
+'use strict';
+
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 var fbUtils = {};
@@ -2518,49 +2564,3 @@ function formBuilderEventsFn() {
     });
   };
 })(jQuery);
-'use strict';
-
-// Element.remove() polyfill
-
-if (!('remove' in Element.prototype)) {
-  Element.prototype.remove = function () {
-    if (this.parentNode) {
-      this.parentNode.removeChild(this);
-    }
-  };
-}
-
-// Event polyfill
-if (typeof Event !== 'function') {
-  (function () {
-    window.Event = function (evt) {
-      var event = document.createEvent('Event');
-      event.initEvent(evt, true, true);
-      return event;
-    };
-  })();
-}
-
-// Object.assign polyfill
-if (typeof Object.assign != 'function') {
-  Object.assign = function (target) {
-    'use strict';
-
-    if (target == null) {
-      throw new TypeError('Cannot convert undefined or null to object');
-    }
-
-    target = Object(target);
-    for (var index = 1; index < arguments.length; index++) {
-      var source = arguments[index];
-      if (source != null) {
-        for (var key in source) {
-          if (Object.prototype.hasOwnProperty.call(source, key)) {
-            target[key] = source[key];
-          }
-        }
-      }
-    }
-    return target;
-  };
-}
