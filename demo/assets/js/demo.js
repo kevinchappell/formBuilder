42e53f8a787a9e6d7aa4e36a69dd7898f63b8f03
Kevin Chappell <kevin.b.chappell@gmail.com>
2016-09-19 22:50:38 +0100

    Update demo

--- demo/assets/js/demo.js
+++ demo/assets/js/demo.js
@@ -5,15 +5,7 @@ jQuery(document).ready(function($) {
     formData = window.sessionStorage.getItem('formData'),
     editing = true,
     fbOptions = {
-      dataType: 'json',
-      typeUserAttrs: {
-        text: {
-          pattern: {
-            label: 'Pattern',
-            value: '[\+]\d{2}[\(]\d{2}[\)]\d{4}[\-]\d{4}|\d{3}[\-]\d{3}[\-]\d{4}'
-          }
-        }
-      }
+      dataType: 'json'
     };
 
   if (formData) {
