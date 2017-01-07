4df5c34a9cfd449a9a98f5b70186421bafcc1d1a
Dietmar Lembke <lembke@lcc.ch>
2017-01-07 17:28:26 +0100

    make check on select which is required work
    with out the empty value a select which has no user  selection the check passes, same applies in form-builder.js

--- dist/form-render.js
+++ dist/form-render.js
@@ -451,7 +451,7 @@ fbUtils.fieldRender = function (fieldData, opts) {
       if (fieldOptions) {
 
         if (fieldData.placeholder) {
-          optionsMarkup += '<option disabled selected>' + fieldData.placeholder + '</option>';
+          optionsMarkup += '<option value disabled selected>' + fieldData.placeholder + '</option>';
         }
 
         for (var i = 0; i < fieldOptions.length; i++) {
