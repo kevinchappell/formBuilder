0cbd5910224e56c95db3a792129ff8f3f84d65b6
Hayri Can BARÇIN <hayricanbarcin@gmail.com>
2017-01-25 14:32:11 +0300

    InputSets name attribute problem
     it was't taking the name attribute from inputSets . It was using nameAttr(field) in all cases.

--- src/js/form-builder.js
+++ src/js/form-builder.js
@@ -532,7 +532,7 @@
         field = Object.assign({}, $field);
       }
 
-      field.name = isNew ? nameAttr(field) : ( field.name || nameAttr(field) );
+      field.name = isNew ? ( field.name || nameAttr(field) ) : nameAttr(field);
 
       if (isNew && utils.inArray(field.type, ['text', 'number', 'file', 'select', 'textarea'])) {
         field.className = 'form-control'; // backwards compatibility
