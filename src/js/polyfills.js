33be791ef15c82ec1bb4fd5fe64038c2c00b6171
Kevin Chappell <kevin.b.chappell@gmail.com>
2017-02-14 02:36:31 +0000

    Add polyfill for ie11 #365

--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -45,6 +45,34 @@ function polyfills() {
       return target;
     };
   }
+
+
+  // Reference: http://es5.github.io/#x15.4.4.18
+  if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(callback) {
+      let T, k;
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+      let O = Object(this);
+      let len = O.length >>> 0;
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+      }
+      if (arguments.length > 1) {
+        T = arguments[1];
+      }
+      k = 0;
+      while (k < len) {
+        let kValue;
+        if (k in O) {
+          kValue = O[k];
+          callback.call(T, kValue, k, O);
+        }
+        k++;
+      }
+    };
+  }
 }
 
 module.exports = polyfills();
