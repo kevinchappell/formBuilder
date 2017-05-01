cad56e8cf8f7a2b8861bea7132cab05ca3000680
Brent Kelly <brent@zeald.com>
2017-05-02 00:22:54 +1200

    upgrade custom control class to support definitions of each custom control

--- src/js/control/custom.js
+++ src/js/control/custom.js
@@ -1,4 +1,5 @@
 import {control} from '../control';
+import mi18n from 'mi18n';
 
 /**
  * Support for custom controls
@@ -10,13 +11,55 @@ export class controlCustom extends control {
    * Override the register method to allow passing 'templates' configuration data
    * @param templates an object/hash of template data as defined http://formbuilder.readthedocs.io/en/latest/formBuilder/options/templates/
    */
-  static register(templates) {
+  static register(templates, fields=[]) {
+    if (!controlCustom.def) {
+      controlCustom.def = {
+        icon: {},
+        i18n: {}
+      };
+    }
 
     // store the template data against a static property
     controlCustom.templates = templates;
 
+    // prepare i18n locale definition
+    let locale = mi18n.locale;
+    if (!controlCustom.def.i18n[locale]) {
+      controlCustom.def.i18n[locale] = {};
+    }
+
+    // build the control label & icon definitions
+    for (let field of fields) {
+      let type = field.type;
+      field.attrs = field.attrs || {};
+      if (!type) {
+        if (!field.attrs.type) {
+          this.error('Ignoring invalid custom field definition. Please specify a type property.');
+          continue;
+        }
+        type = field.attrs.type;
+      }
+
+      // ensure there is a template defined for this field
+      if (!templates[type]) {
+        this.error('Error while registering custom field: ' + field + '. Unable to find a related defined template.');
+      }
+
+      // map label & icon
+      controlCustom.def.i18n[locale][type] = field.label;
+      controlCustom.def.icon[type] = field.icon;
+    }
+
     // register each defined template against this class
     control.register(Object.keys(templates), controlCustom);
+  }
+
+  /**
+   * Class configuration - return the icons & label translations defined in register
+   * @returns definition object
+   */
+  static get definition() {
+    return controlCustom.def;
   }
 
   /**
