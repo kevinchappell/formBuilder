0b8b6976be357c5400edf6ac50765fe0a1a17db8
Kevin Chappell <kevin.b.chappell@gmail.com>
2017-05-13 00:15:03 +0100

    Return event so dispatch functions correctly

--- src/js/events.js
+++ src/js/events.js
@@ -3,17 +3,22 @@
  * @return {Object} various events to be trigger
  */
 
-// var events = {
-//   loaded: new Event('loaded'),
-//   viewData: new Event('viewData'),
-//   userDeclined: new Event('userDeclined'),
-//   modalClosed: new Event('modalClosed'),
-//   modalOpened: new Event('modalOpened'),
-//   formSaved: new Event('formSaved'),
-//   fieldAdded: new Event('fieldAdded'),
-//   fieldRemoved: new Event('fieldRemoved'),
-//   fieldRendered: new Event('fieldRendered')
-// };
+/**
+ * Wrapper to deal with ie11
+ * @param {String} eventName
+ * @return {Event} event
+ */
+function createNewEvent(eventName) {
+  let event;
+  if (typeof Event === 'function') {
+    event = new Event(eventName);
+  } else {
+    event = document.createEvent('Event');
+    event.initEvent(eventName, true, true);
+  }
+
+  return event;
+}
 
 const events = {};
 
