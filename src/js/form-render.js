e326af6f5a807b9896c859c48b14a92edc23b93e
Brent Kelly <brent@zeald.com>
2017-05-23 02:01:42 +1200

    This is a cool new feature of formRender that allows you to render just a single field (#475)
    * ability to call .fieldRender(data) on a jQuery element & render just one field into that element. Uses new renderSingle method
    
    * fixing webpack jsdoc errors
    
    * ability to pass an object rather than json for the formData
    
    * rename to more suitable name, support forcing template so it only outputs the control by default but ability to override
    
    * ability to force the layout template to use for rendering programatically

--- src/js/form-render.js
+++ src/js/form-render.js
@@ -10,7 +10,6 @@ import controlCustom from './control/custom';
 
 /**
  * FormRender Class
- * @todo: prototype.js compatibility
  */
 class FormRender {
 
@@ -66,7 +65,10 @@ class FormRender {
         json: formData => window.JSON.parse(formData)
       };
 
-      this.options.formData = setData[this.options.dataType](this.options.formData) || false;
+      // if the user hasn't passed a pre-parsed formData object, parse it according to the specified dataType
+      if (typeof this.options.formData !== 'object') {
+        this.options.formData = setData[this.options.dataType](this.options.formData) || false;
+      }
     })();
 
     // ability for controls to have their own configuration / options of the format control identifier (type, or type.subtype): {options}
@@ -79,6 +81,51 @@ class FormRender {
     if (Object.keys(this.options.templates).length) {
       controlCustom.register(this.options.templates);
     }
+
+    /**
+     * Extend Element prototype to allow us to append fields
+     *
+     * @param  {Object} fields Node elements
+     */
+    if (typeof Element.prototype.appendFormFields !== 'function') {
+      Element.prototype.appendFormFields = function(fields) {
+        let element = this;
+        if (!Array.isArray(fields)) {
+          fields = [fields];
+        }
+        fields.forEach(field => {
+          element.appendChild(field);
+          field.dispatchEvent(events.fieldRendered);
+        });
+      };
+    }
+
+    /**
+     * Extend Element prototype to remove content
+     */
+    if (typeof Element.prototype.emptyContainer !== 'function') {
+      Element.prototype.emptyContainer = function() {
+        let element = this;
+        while (element.lastChild) {
+          element.removeChild(element.lastChild);
+        }
+      };
+    }
+  }
+
+  /**
+   * Clean up passed object configuration to prepare for use with the markup function
+   * @param {Object} field - object of field configuration
+   * @return {Object} sanitized field object
+   */
+  santizeField(field) {
+    let sanitizedField = Object.assign({}, field);
+    sanitizedField.className = field.className || field.class || null;
+    delete sanitizedField.class;
+    if (field.values) {
+      field.values = field.values.map(option => utils.trimObj(option));
+    }
+    return utils.trimObj(sanitizedField);
   }
 
   /**
@@ -90,50 +137,10 @@ class FormRender {
     const formRender = this;
     let opts = this.options;
 
-    /**
-     * Extend Element prototype to allow us to append fields
-     *
-     * @param  {Object} fields Node elements
-     */
-    Element.prototype.appendFormFields = function(fields) {
-      let element = this;
-      fields.forEach(field => {
-        element.appendChild(field);
-        field.dispatchEvent(events.fieldRendered);
-      });
-    };
-
-    /**
-     * Extend Element prototype to remove content
-     */
-    Element.prototype.emptyContainer = function() {
-      let element = this;
-      while (element.lastChild) {
-        element.removeChild(element.lastChild);
-      }
-    };
-
     let runCallbacks = function() {
       if (opts.onRender) {
         opts.onRender();
       }
-    };
-
-    /**
-     * Clean up passed object configuration to prepare for use with the markup function
-     * @param {Object} field - object of field configuration
-     * @return {Object} sanitized field object
-     */
-    let santizeField = field => {
-      let sanitizedField = Object.assign({}, field);
-      sanitizedField.className = field.className || field.class || null;
-      delete sanitizedField.class;
-
-      if (field.values) {
-        field.values = field.values.map(option => utils.trimObj(option));
-      }
-
-      return utils.trimObj(sanitizedField);
     };
 
     /**
@@ -152,8 +159,7 @@ class FormRender {
       let engine = new opts.layout(opts.layoutTemplates);
       for (let i = 0; i < opts.formData.length; i++) {
         let fieldData = opts.formData[i];
-        let sanitizedField = santizeField(fieldData);
-        // let type = fieldData.subtype || fieldData.type;
+        let sanitizedField = this.santizeField(fieldData);
 
         // determine the control class for this type, and then process it through the layout engine
         let controlClass = control.getClass(fieldData.type, fieldData.subtype);
@@ -195,6 +201,30 @@ class FormRender {
 
     return formRender;
   }
+
+  /**
+   * Render a single control / field
+   * Expects only a single field configuration to be set in opt.formData
+   * @param {Object} element - an optional DOM element to render the field into - if not specified will just return the rendered field - note if you do this you will need to manually call element.dispatchEvent('fieldRendered') on the returned element when it is rendered into the DOM
+   * @return {Object} the formRender object
+   */
+  renderControl(element = null) {
+    let opts = this.options;
+    let fieldData = opts.formData;
+    if (!fieldData || Array.isArray(fieldData)) {
+      throw new Error('To render a single element, please specify a single object of formData for the field in question');
+    }
+    let sanitizedField = this.santizeField(fieldData);
+
+    // determine the control class for this type, and then build it
+    let engine = new opts.layout();
+    let controlClass = control.getClass(fieldData.type, fieldData.subtype);
+    let forceTemplate = opts.forceTemplate || 'hidden'; // support the ability to override what layout template the control is rendered using. This can be used to output the whole row (including label, help etc) using the standard templates if desired.
+    let field = engine.build(controlClass, sanitizedField, forceTemplate);
+    element.appendFormFields(field);
+    opts.notify.success(opts.messages.formRendered);
+    return this;
+  }
 }
 
 (function($) {
@@ -205,5 +235,23 @@ class FormRender {
       elems[i].dataset.formRender = formRender;
       return formRender.render(elems[i]);
     });
+  };
+
+  /**
+   * renders an individual field into the current element
+   * @param {Object} data - data structure for a single field output from formBuilder
+   * @param {Object} options - optional subset of formRender options - doesn't support container or other form rendering based options.
+   * @return {DOMElement} the rendered field
+   */
+  $.fn.controlRender = function(data, options = {}) {
+    options.formData = data;
+    options.dataType = typeof data === 'string' ? 'json' : 'xml';
+    let formRender = new FormRender(options);
+    let elems = this;
+    elems.each(function(i) {
+      elems[i].dataset.formRender = formRender;
+      return formRender.renderControl(elems[i]);
+    });
+    return elems;
   };
 })(jQuery);
