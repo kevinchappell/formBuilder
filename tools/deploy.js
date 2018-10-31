2b1392f4af6c38d09bdd91d921598f644e551420
Kevin Chappell <kevin.b.chappell@gmail.com>
2018-10-31 12:34:02 -0700

    Add deploy script

---
+++ tools/deploy.js
@@ -0,0 +1,19 @@
+import { exec } from 'child_process'
+
+const onFinish = (err, cb) => {
+  if (!err) {
+    cb()
+  } else {
+    console.error(err)
+  }
+}
+
+// Deploy the demo and site.
+const deploy = async () => exec('git push origin $(git subtree split --prefix demo $(git rev-parse --abbrev-ref HEAD)):origin/gh-pages --force', err => {
+  onFinish(err, () => {
+    console.log('Demo successfully deployed')
+    // exec('cd site && yarn deploy && cd ../', err => onFinish(err, () => console.log('Site successfully deployed')))
+  })
+})
+
+export default deploy
