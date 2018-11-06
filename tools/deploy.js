c3f4051ee8a45168cd6724bbd88528a96fa8c12d
Kevin Chappell <kevin.b.chappell@gmail.com>
2018-11-06 01:26:03 -0800

    add npm script deploy:site

--- tools/deploy.js
+++ tools/deploy.js
@@ -9,11 +9,7 @@ const onFinish = (err, cb) => {
   }
 };
 
-const deploySiteCommands = ['yarn docs', 'cd site/', 'yarn deploy', 'cd ../']
-
 // Deploy the demo and site.
 exec('git push origin $(git subtree split --prefix demo $(git rev-parse --abbrev-ref HEAD)):gh-pages --force', err =>
   onFinish(err, () => console.log('Demo successfully deployed'))
 );
-
-exec(deploySiteCommands.join(' && '), err => onFinish(err, () => console.log('Site successfully deployed')));
