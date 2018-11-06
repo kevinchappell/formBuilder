19168709ec4b37f045ba0280c923db278c09fe51
Kevin Chappell <kevin.b.chappell@gmail.com>
2018-11-05 23:06:08 -0800

    fix(cd): update readme, have deploy script return, run deploy script directly

--- tools/deploy.js
+++ tools/deploy.js
@@ -2,26 +2,25 @@ import { exec } from 'child_process'
 
 const onFinish = (err, cb) => {
   if (!err) {
-    cb()
+    return cb()
   } else {
     console.error(err)
+    return process.exit(1)
   }
 }
 
-const deploySiteCommands = [
-  'git submodule update --init --recursive',
-  'yarn docs',
-  'cd site',
-  'yarn deploy',
-  'cd ../'
-]
+const deploySiteCommands = ['git submodule update --init --recursive', 'yarn docs', 'cd site', 'yarn deploy', 'cd ../']
 
 // Deploy the demo and site.
-const deploy = async () => exec('git push origin $(git subtree split --prefix demo $(git rev-parse --abbrev-ref HEAD)):gh-pages --force', err => {
-  onFinish(err, () => {
-    console.log('Demo successfully deployed')
-    exec(deploySiteCommands.join(' && '), err => onFinish(err, () => console.log('Site successfully deployed')))
-  })
-})
+const deploy = () => exec(
+  'git push origin $(git subtree split --prefix demo $(git rev-parse --abbrev-ref HEAD)):gh-pages --force',
+  err =>
+    onFinish(err, () => {
+      console.log('Demo successfully deployed')
+      return exec(deploySiteCommands.join(' && '), err =>
+        onFinish(err, () => console.log('Site successfully deployed'))
+      )
+    })
+)
 
 export default deploy
