c15ee4309a4e30f7522166361ad3cef9733b514d
Kevin Chappell <kevin.b.chappell@gmail.com>
2018-11-05 23:41:49 -0800

    update build script

--- tools/deploy.js
+++ tools/deploy.js
@@ -1,20 +1,20 @@
-const { exec } = require('child_process')
+const { exec } = require('child_process');
 
 const onFinish = (err, cb) => {
   if (!err) {
-    return cb()
+    return cb();
   } else {
-    console.error(err)
-    return process.exit(1)
+    console.error(err);
+    return process.exit(1);
   }
 }
 
-const deploySiteCommands = ['git submodule update --init --recursive', 'yarn docs', 'cd site', 'yarn deploy', 'cd ../']
+// const deploySiteCommands = ['git submodule update --init --recursive', 'yarn docs', 'cd site', 'yarn deploy', 'cd ../'];
 
 // Deploy the demo and site.
 exec('git push origin $(git subtree split --prefix demo $(git rev-parse --abbrev-ref HEAD)):gh-pages --force', err =>
   onFinish(err, () => {
-    console.log('Demo successfully deployed')
-    return exec(deploySiteCommands.join(' && '), err => onFinish(err, () => console.log('Site successfully deployed')))
+    console.log('Demo successfully deployed');
+    // return exec(deploySiteCommands.join(' && '), err => onFinish(err, () => console.log('Site successfully deployed')))
   })
-)
+);
