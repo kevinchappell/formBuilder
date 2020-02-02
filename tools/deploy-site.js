f6ffef23d4ce105cc6673c43d2396e8f5f0efd8b
Kevin Chappell <kevin.b.chappell@gmail.com>
2020-02-01 21:23:23 -0800

    update deploy-site

--- tools/deploy-site.js
+++ tools/deploy-site.js
@@ -2,15 +2,14 @@ const { exec } = require('child_process')
 const pkg = require('../package.json')
 
 const commands = [
+  `cd ${process.env.FORMBUILDER_WEBSITE_DIR || 'site'}/`,
   `npm version ${pkg.version}`,
   'git push origin master',
 ]
 
-const cwd = process.env.FORMBUILDER_WEBSITE_DIR || 'site'
-
 // try to deploy the demo
 try {
-  exec(commands.join(' && '), { cwd }, err => !err && console.log('Site successfully deployed'))
+  exec(commands.join(' && '), err => !err && console.log('Site successfully deployed'))
 } catch (e) {
   console.error(e)
 }
