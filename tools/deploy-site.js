39a73fb826fe81eae5398480d52d2f7a34267eaf
Kevin Chappell <kevin.b.chappell@gmail.com>
2023-08-20 01:07:04 -0700

    fix: publish github action

--- tools/deploy-site.js
+++ tools/deploy-site.js
@@ -1,8 +1,12 @@
 const { exec } = require('child_process')
 const pkg = require('../package.json')
 
+const siteReo = pkg.repository.url.replace(/.git$/, '-site.git')
+const siteDir = process.env.FORMBUILDER_WEBSITE_DIR || 'site'
+
 const commands = [
-  `cd ${process.env.FORMBUILDER_WEBSITE_DIR || 'site'}/`,
+  `git clone ${siteReo} ${siteDir}`,
+  `cd ${siteDir}/`,
   `npm version ${pkg.version}`,
   'git push origin master',
 ]
