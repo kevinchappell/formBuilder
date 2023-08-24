92a8a720ce3b4c92414fd91a6a4ac10fe96c3ba1
James Lucas <james@lucas.net.au>
2023-08-24 11:38:38 +1000

    fix: Array of strings was removed from support in TinyMCE6, use a plugin definition format supported my all versions (https://www.tiny.cloud/docs/tinymce/6/migration-from-5x/#plugin-loading-format-change)

--- src/js/control/textarea.tinymce.js
+++ src/js/control/textarea.tinymce.js
@@ -45,11 +45,9 @@ export default class controlTinymce extends controlTextarea {
     this.editorOptions = {
       height: 250,
       paste_data_images: true,
-      plugins: [
-        'advlist autolink lists link image charmap print preview anchor',
-        'searchreplace visualblocks code fullscreen',
-        'insertdatetime media table contextmenu paste code',
-      ],
+      plugins: ['advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'print', 'preview', 'anchor',
+               'searchreplace', 'visualblocks', 'code', 'fullscreen',
+               'insertdatetime', 'media', 'table', 'contextmenu', 'paste', 'code'],
       toolbar:
         'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | table',
     }
