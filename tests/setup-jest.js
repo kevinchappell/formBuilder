e416363a49f999dc9c63c5e35e420145daa321d8
James Lucas <james@lucas.net.au>
2023-10-17 12:28:59 +1100

    Silence the error generated by jsdom xhr when loading assets/lang/en-US.lang when a path to language files is not set

--- tests/setup-jest.js
+++ tests/setup-jest.js
@@ -1,8 +1,15 @@
-
-
 import $ from 'jquery'
 global.$ = $
 global.jQuery = $
 
 const langFiles = require('formbuilder-languages')
 global.FB_EN_US = langFiles['en-US']
+
+/** Hack to silence errors from default request to assets/lang/en-US.lang **/
+const originalConsoleError = console.error
+const jsDomCssError = 'Error: AggregateError'
+console.error = (...params) => {
+  if (!params.find(p => p.toString().includes(jsDomCssError))) {
+    originalConsoleError(...params)
+  }
+}
