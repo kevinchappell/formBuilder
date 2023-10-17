4be28604cf18c62f9f88019aea0db0299172af03
James Lucas <james@lucas.net.au>
2023-10-17 13:04:08 +1100

    multiple XHR errors need to be ignored

--- tests/setup-jest.js
+++ tests/setup-jest.js
@@ -7,9 +7,12 @@ global.FB_EN_US = langFiles['en-US']
 
 /** Hack to silence errors from default request to assets/lang/en-US.lang **/
 const originalConsoleError = console.error
-const jsDomCssError = 'Error: AggregateError'
+const jsIgnoreErrors = [
+  'Error: AggregateError',
+  'Error: connect ECONNREFUSED ::1:80',
+]
 console.error = (...params) => {
-  if (!params.find(p => p.toString().includes(jsDomCssError))) {
+  if (!params.find(p => jsIgnoreErrors.find(ignore => p.toString().includes(ignore)))) {
     originalConsoleError(...params)
   }
 }
