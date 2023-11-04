612d0093217fe98b0f1f79f74afecb977e3b724f
James Lucas <james@lucas.net.au>
2023-11-04 14:37:38 +1100

    fix: add additional row cleanup check to ensure no artifacts are left in the DOM

--- src/js/form-builder.js
+++ src/js/form-builder.js
@@ -1369,6 +1369,7 @@ function FormBuilder(opts, element, $) {
           h.save.call(h)
         }
 
+        checkRowCleanup()
         ResetAllInvisibleRowPlaceholders()
 
         const listFieldItem = $(ui.item).find('li')
