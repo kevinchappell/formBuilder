d3dcab590d6834a3df7ebfb76aef7802ac85887e
kevinchappell <kevin@chappell.dev>
2024-07-07 22:47:49 +0000

    Deploying to gh-pages from @ kevinchappell/formBuilder@92384e8ba6a2a990386ce3dd19a6e85863220141 🚀

--- assets/lang/index.js
+++ assets/lang/index.js
@@ -1,14 +1,25 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('node:fs')
+const path = require('node:path')
 const { default: mi18n } = require('mi18n')
 
+/**
+ * Represents a collection of language files.
+ * @typedef {Object} LangFiles
+ * @property {Object} [locale] - The language locale.
+ * @property {string} [locale.langKey] - The language key.
+ */
+
+/**
+ * Reads and processes language files from the specified directory.
+ * @type {LangFiles}
+ */
 const langFiles = fs.readdirSync(__dirname).reduce((acc, lang) => {
   if (!/.lang$/.test(lang)) {
     return acc
   }
   const langFile = fs.readFileSync(path.resolve(__dirname, lang)).toString()
   const fileName = path.basename(lang)
-  const locale = fileName.substr(0, fileName.indexOf('.'))
+  const locale = fileName.substring(0, fileName.indexOf('.'))
   acc[locale] = mi18n.processFile(langFile)
   return acc
 }, {})
