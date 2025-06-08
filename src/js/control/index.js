b868f524b0a5b64898a8038397b197fee4d0f3ae
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2025-06-08 10:39:47 -0400

    Auto Registration

--- src/js/control/index.js
+++ src/js/control/index.js
@@ -1,13 +1,37 @@
-import controlAutocomplete from './autocomplete'
-import controlButton from './button'
-import controlCustom from './custom'
-import controlHidden from './hidden'
-import controlParagraph from './paragraph'
-import controlSelect from './select'
-import controlText from './text'
-import controlTextarea from './textarea'
-import controlTinymce from './textarea.tinymce'
-import controlQuill from './textarea.quill'
+import '../../js/plugin_system/pluginLoader';
+
+import controlAutocomplete from './autocomplete';
+import controlButton from './button';
+import controlCustom from './custom';
+import controlHidden from './hidden';
+import controlParagraph from './paragraph';
+import controlSelect from './select';
+import controlText from './text';
+import controlTextarea from './textarea';
+import controlTinymce from './textarea.tinymce';
+import controlQuill from './textarea.quill';
+
+import { getAllPlugins } from '../plugin_system/pluginRegistry';
+
+const baseControls = {
+  text: controlText,
+  paragraph: controlParagraph,
+  textarea: controlTextarea,
+  number: controlText, 
+};
+
+const pluginControls = {};
+const allPlugins = getAllPlugins();
+
+
+Object.entries(allPlugins).forEach(([name, pluginObj]) => {
+  if (pluginObj.type === 'control' && typeof pluginObj.plugin === 'function') {
+    const controlClass = pluginObj.plugin(baseControls);
+    pluginControls[name] = controlClass;
+    console.log(`Plugin control loaded: ${name}`);
+  }
+});
+
 
 export default {
   controlAutocomplete,
@@ -20,4 +44,5 @@ export default {
   controlTextarea,
   controlTinymce,
   controlQuill,
-}
+  ...pluginControls 
+};
